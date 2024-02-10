8e6247f96fa3f13b434467d4714d663308311cd2
Blazeblue-cypher <82416603+Blazeblue-cypher@users.noreply.github.com>
2024-02-10 18:08:42 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import { ColorModeContext, useMode } from "./theme";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from './scenes/global/Sidebar';
-//import Dashboard from './scenes/dashboard';
-//import Team from './scenes/team';
-//import Invoices from './scenes/invoices';
-//import Contacts from './scenes/contacts';
+import Dashboard from './scenes/dashboard';
+import Team from './scenes/team';
+import Invoices from './scenes/invoices';
+import Contacts from './scenes/contacts';
+import Form from './scenes/form';
 //import Bar from './scenes/bar';
-//import Form from './scenes/form';
 //import Line from './scenes/line';
 //import Pie from './scenes/pie';
 //import FAQ from './scenes/faq';
 //import Geography from './scenes/geography';
-//import Calendar from './scenes/calendar';
+import Calendar from './scenes/calendar';
 
 
 
@@ -31,16 +31,17 @@ function App() {
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
-              {/*<Route path='/' element={<Dashboard />} />*/}
-              {/*<Route path='/contacts' element={<Contacts />} />*/}
-              {/*<Route path='/invoices' element={<Invoices />} />*/}
+              <Route path='/' element={<Dashboard />} />
+              <Route path='/contacts' element={<Contacts />} />
+              <Route path="/team" element={<Team />} />
+              <Route path='/invoices' element={<Invoices />} />
               {/*<Route path='/bar' element={<Bar />} />*/}
-              {/*<Route path='/form' element={<Form />} />*/}
+              <Route path='/form' element={<Form />} />
               {/*<Route path='/pie' element={<Pie />} />*/}
               {/*<Route path='/line' element={<Line />} />*/}
               {/*<Route path='/faq' element={<FAQ />} />*/}
               {/*<Route path='/geography' element={<Geography />} />*/}
-              {/*<Route path='/calendar' element={<Calendar />} />*/}
+              <Route path='/calendar' element={<Calendar />} />
             </Routes>
           </main>
         </div>
